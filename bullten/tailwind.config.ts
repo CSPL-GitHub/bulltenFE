a36a1ecead64ac21e134f351dfe0df6c080ea5ee
surwaseonkar8999 <onkar.surwase@consociatesolutions.com>
2024-08-22 17:41:09 +0530

    "onkar-push-22-aug"

--- bullten/tailwind.config.ts
+++ bullten/tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
         "bullt-primary": "#000000", //black
         "bullt-secondary": "#FAFAFA", //white
         "bullt-tertiary": "#f69b00", //yellow
-        "bullt-quaternary": "#191970", // dark blue
+        "bullt-quaternary": "#261c3a", // dark blue
         "bullt-quinary": "#4529a3", //purple
         "bullt-background": "#FAFAFA", //white
         "bullt-text-primary": "#01204E", //dark blue
@@ -22,7 +22,14 @@ const config: Config = {
         "bullt-text-quinary": "#f69b00",
       },
       backgroundImage: {
-        "custom-gradient": "linear-gradient(180deg, #8f79d2 0%, #4529a3 100%)",
+        "primary-gradient": "linear-gradient(90deg, #4529a3 0%, #261c3a 100%)", // Black to dark blue
+        "secondary-gradient":
+          "linear-gradient(270deg, #FAFAFA 0%, #f69b00 100%)", // White to yellow
+        "tertiary-gradient": "linear-gradient(90deg, #f69b00 0%, #4529a3 100%)", // Yellow to purple
+        "quaternary-gradient":
+          "linear-gradient(90deg, #01204E 0%, #535E73 100%)", // Dark blue to grayish blue
+        "quinary-gradient": "linear-gradient(180deg, #4529a3 0%, #8f79d2 100%)", // Purple to lighter purple
+        "senary-gradient": "linear-gradient(90deg, #FFFFFF 0%, #FFFFFF 100%)", // White to light blue
       },
     },
   },
