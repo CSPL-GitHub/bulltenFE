8212023fd8eff2810a7414068679e124b4d35851
Adham Kazi <adhamul.kazi@consociatesolutions.com>
2024-08-22 20:14:31 +0530

    Adham-testimonial-with-api-22-Aug-2014

--- bullten/components/ServerSideComponents/HomePageComponents/TestimonialsComponents/TestimonialsCard.tsx
+++ bullten/components/ServerSideComponents/HomePageComponents/TestimonialsComponents/TestimonialsCard.tsx
@@ -12,28 +12,30 @@ export default function TestimonialsCard({
   testimonial: any;
 }) {
   return (
-    <div className="group w-full min-h-64 relative bg-bullt-secondary rounded-md border-t-4 border-bullt-quaternary hover:rounded-md shadow-md mx-auto mt-12 mb-10  transition-colors duration-300">
+    <div className="group w-full min-h-60 relative bg-bullt-secondary rounded-md border-t-4 border-bullt-quaternary hover:rounded-md shadow-md mx-auto mt-12 mb-10  transition-colors duration-300">
       <div className="absolute inset-x-0 top-0 flex justify-center transform -translate-y-1/2">
         <div className="w-24 h-24 relative flex justify-center bg-bullt-quaternary  group-hover:bg-bullt-secondary rounded-full shadow-md items-center transition-colors duration-300">
-          <FaQuoteRight className="text-bullt-secondary group-hover:text-bullt-quaternary text-4xl transition-colors duration-300" />
+          <FaQuoteRight className="text-bullt-secondary  group-hover:text-bullt-quaternary text-4xl transition-colors duration-300" />
         </div>
       </div>
       <div className="w-full h-full pt-14 px-4 pb-3 flex flex-col justify-between items-start gap-2 group-hover:bg-bullt-quaternary group-hover:rounded-md  transition-colors duration-300">
-        {testimonial?.text ? (
+        {testimonial?.description ? (
           <ParaGraphText
             paddingTop={3}
+            alignmentType={1}
             hoverEffect="group-hover:text-bullt-secondary transition-colors duration-300"
           >
-            {testimonial?.text}
+            {testimonial?.description}
           </ParaGraphText>
         ) : null}
         <div className="flex flex-row gap-2 justify-center items-start">
-          {testimonial?.testimonialsImageUrl ? (
+          {testimonial?.client_image ? (
             <div className="w-14 h-14 relative">
-              <img
-                src={testimonial?.testimonialsImageUrl}
-                alt="Profile"
+              <Image
+                src={`${process.env.NEXT_PUBLIC_BASE_URL}${testimonial?.client_image}`}
+                alt={testimonial?.alt_text}
                 className="w-full h-full absolute top-0 start-0 rounded-full object-cover"
+                fill={true}
               />
             </div>
           ) : null}
@@ -41,6 +43,7 @@ export default function TestimonialsCard({
             {testimonial?.name ? (
               <SloganHeadingComponent
                 paddingTop={1}
+                alignmentType={1}
                 hoverEffect="group-hover:text-bullt-secondary transition-colors duration-300"
               >
                 {testimonial?.name}
