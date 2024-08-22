474c87539af4263e6ced01c7a575f0968532914b
SURAJJADHAV75 <suraj.jadhav@consociatesolutions.com>
2024-08-22 20:33:09 +0530

    "suraj"

--- bullten/app/page.tsx
+++ bullten/app/page.tsx
@@ -1,4 +1,4 @@
-import { HomePageBannerApi, TestimonialsApi } from "@/apis/HomePageApis";
+import { HomePageBannerApi } from "@/apis/HomePageApis";
 import BannerSlider from "@/components/ServerSideComponents/BannerComponent/BannerSlider";
 import FaqSection from "@/components/ServerSideComponents/HomePageComponents/FaqsectionComponent";
 import SupportSection from "@/components/ServerSideComponents/HomePageComponents/SupportComponent";
@@ -12,12 +12,13 @@ import CounterComponent from "@/components/ServerSideComponents/HomePageComponen
 import TestimonialSlider from "@/components/ServerSideComponents/HomePageComponents/TestimonialsComponents/TestimonialSlider";
 export default async function Home() {
   const homePageBannerContentApi = await HomePageBannerApi();
-  const TestimonialsContent = await TestimonialsApi();
-
   return (
     <main className="w-full">
       <Suspense fallback={<Skeleton height={"50%"} width={"100%"} />}>
-        <BannerSlider banners={homePageBannerContentApi.result.banners} />
+        {homePageBannerContentApi?.result?.Active === &&
+          homePageBannerContentApi?.result?.banners?.length > 0 && (
+            <BannerSlider banners={homePageBannerContentApi?.result?.banners} />
+          )}
       </Suspense>
       <div className="container flex flex-col items-center justify-center mx-auto">
         <Suspense fallback={<Skeleton height={"50%"} width={"100%"} />}>
@@ -32,9 +33,9 @@ export default async function Home() {
         <Suspense fallback={<Skeleton height={"50%"} width={"100%"} />}>
           <FaqSection />
         </Suspense>
-        <Suspense fallback={<Skeleton height={"50%"} width={"100%"} />}>
-          <TestimonialSlider TestimonialsContent={TestimonialsContent} />
-        </Suspense>
+        {/* <Suspense fallback={<Skeleton height={"50%"} width={"100%"} />}>
+          <TestimonialSlider />
+        </Suspense> */}
         <Suspense fallback={<Skeleton height={"50%"} width={"100%"} />}>
           <CounterComponent />{" "}
         </Suspense>
